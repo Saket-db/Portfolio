58df2818a58ddb8996cebe8cdcca78ec22df3036
Saket-db <dbsaket04@gmail.com>
2025-01-12 20:17:36 +0530

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,57 +1,155 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link as LinkR, NavLink} from "react-router-dom";
+import {Link as LinkR} from "react-router-dom";
+import {Bio} from "../data/constants.js";
+import {  MenuRounded } from "@mui/icons-material";
+import { useState } from 'react';
 
-const NavbarContainer = styled.div`
+const Nav = styled.div`
 background-color: ${({theme}) => theme.bg};
-height: 75px;
+height: 65px;
 align-items: center;
 justify-content: center;
 top: 0;
 position: sticky;
+display: flex;
 font-size:1 rem;
 z-index: 20;
 color: white;
 `;
+
+const NavbarContainer = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 65px;
+  padding: 0 24px;
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
+
+
 const NavLogo = styled(LinkR)`
-padding: 0 6px;
-text-decoration: none;
-color: inherit;`
-;
+  padding: 0 6px;
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: inherit;
+  display: flex;
+  align-items: center;
+  transition: transform 0.7s ease, font-size 0.7s ease;
+
+  &:hover {
+    transform: scale(1.1);
+    font-size: 1.7rem;
+  }
+`;
+
 
 const NavItems = styled.ul`
-width: 100%;
-display: flex;
-align-items: center;
-justify-content: center;
-list-type:none;
-gap: 27px;
-padding: 0 6px;
-text-decoration: none;
-`
-;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+  gap: 32px;
+  padding: 0;
+  // margin: 0;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
-const NavLink = styled.a`
+
+const NavLink2 = styled.a`
 color ${({theme}) => theme.text_primary};
 font-weight:500;
 cursor: pointer;
-transition: all 0.2s ease-in-out;
+// display: flex;
+transition: all 0.4s ease-in-out;
 text-decoration: none;
-
+&:hover {
+color: ${({ theme })=> theme.primary}}; 
 `
+;
+
+const ButtonContainer = styled.div`
+width: full;
+height: 100%;
+display: flex;
+justify-content:end;
+align-items: center;
+// flex-basis:50%;
+padding:0 6px;
+@media screen and (max-width:768px){
+display: none;
+}
+`
+;
+
+const GithubButton = styled.a`
+  border: 1px solid ${({ theme }) => theme.primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  cursor: pointer;
+  border-radius: 13px;
+  padding: 4px 10px;
+  text-decoration: none;
+  font-weight: 500;
+  color: ${({ theme }) => theme.primary};
+  transition: all 0.4s ease-in-out;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.text_primary};
+  }
+`;
+
+
+const MobileIcon = styled.div`
+display: flex;
+align-items:center;
+height: 100%;
+display: none;
+color: ${({theme}) => theme.text_primary};
+@media screen and (max-width:768px){
+display: block;
+margin-top: 40px;
+}
+`
+;
 
 const Navbar = () => {
-  return <NavbarContainer>
-    <NavLogo to = "/" >Saket Dwaraka Bhamidipaati
-    <NavItems>
-        <NavLink href = "">About</NavLink>
-        <NavLink href = "">Skills</NavLink>
-        <NavLink href = "">Experience</NavLink>
-        <NavLink href = "">Projects</NavLink>
-        <NavLink href = "">Education</NavLink>
-    </NavItems>
+  const[IsOpen, setisOpen] = useState(false);
+  return (
+  <Nav>
+  <NavbarContainer>
+    <NavLogo to = "/" >Saket DB</NavLogo>
+    <MobileIcon onClick={() => setisOpen(!IsOpen)}>
+      <MenuRounded style={{ color: "inherit"}} />
+    </MobileIcon>
 
-    </NavLogo></NavbarContainer>;
+    <NavItems>
+      
+        <NavLink2 href = "">About</NavLink2>
+        <NavLink2 href = "">Skills</NavLink2>
+        <NavLink2 href = "">Experience</NavLink2>
+        <NavLink2 href = "">Projects</NavLink2>
+        <NavLink2 href = "">Education</NavLink2>
+        
+    <ButtonContainer>
+      <GithubButton href = {Bio.github} target = "_blank">Github Profile</GithubButton>
+    </ButtonContainer>
+    </NavItems>
+    
+    
+    </NavbarContainer>
+    </Nav>
+
+  )  
 }
 
 export default Navbar;
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,20 +1,18 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Saket Dwaraka Bhamidipaati",
   roles: [
     "Full Stack Developer",
-    "Android Developer",
     "UI/UX Designer",
     "Programmer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/Saket-db",
   resume:
     "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+  linkedin: "https://www.linkedin.com/in/saket-dwaraka-bhamidipaati/",
+  twitter: "https://x.com/Saket_db",
+  insta: "https://www.instagram.com/saket_db/",
 };
 
 export const skills = [
