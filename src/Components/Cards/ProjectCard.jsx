47496c6de98f110d1cee0e96b92023ebb8e11397
Saket-db <dbsaket04@gmail.com>
2025-01-19 23:54:11 +0530

    Css_Projects_1

--- src/Components/Cards/ProjectCard.jsx
+++ src/Components/Cards/ProjectCard.jsx
@@ -3,12 +3,72 @@
 import styled from 'styled-components';
 import React from 'react'
 
-const Card = styled.div``;
-const Image = styled.img``;
-const Tags = styled.div``;
-const Title = styled.div``;
+const Card = styled.div`
+width: 330px;
+height: 490px;
+cursor: pointer;
+background-color: ${({ theme }) => theme.card + 'CC'};
+border-radius: 8px;
+box-shadow: box-shadow: rgba(23, 93, 230, 0.35) 0px 8px 24px;
+display: flex;
+flex-direction: column;
+overflow:hidden;
+gap: 10px;
+transition: all 0.5s ease-in-out;
+
+&:hover{
+transform: translate(-10px);
+box-shadow: box-shadow: rgba(23, 93, 230, 0.35) 0px 18px 14px;
+filter: brightness(1.1);
+}
+
+`
+;
+
+const Image = styled.img`
+width: 100%;
+height: 110px;
+background-color: ${({ theme }) => theme.white};
+border-radius: 8px;
+box-shadow: box-shadow: rgba(23, 93, 230, 0.35) 0px 14px 4px;
+// overflow:hidden;
+// padding: 20px 15px;
+// display: flex;
+// flex-direction: column;
+
+`;
+
+
+
+const Tags = styled.div`
+width: 100%;
+diplay: flex;
+align-items: center;
+flex-wrap: wrap;
+gap: 5px;
+margin-top: 4px;
+`;
+
+
+const Title = styled.div`
+font-size: 20px;
+font-weight: 600;
+color: ${({theme}) => theme.tex_secondary};
+
+`;
 const Date = styled.div``;
-const Details = styled.div``;
+
+
+const Details = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+gap: 0px;
+padding: 0px 4px;
+`;
+
+
+
 const Description = styled.div``;
 const Members = styled.div``;
 const Avatar = styled.img``;
